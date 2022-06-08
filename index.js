cbdd7292c19d919119f1d377a4dac76f3aa52940
Юлия Седушкина <sed1452002@gmail.com>
2022-06-08 22:06:43 +0300

    homework done

---
+++ index.js
@@ -0,0 +1,7 @@
+const plus = require("./sum.js");
+const minus = require("./subtraction");
+const multiply = require("./multiplication");
+
+console.log(plus.sum(1, 2));
+console.log(minus.subtraction(4, 2));
+console.log(multiply.multiplication(3, 2));
